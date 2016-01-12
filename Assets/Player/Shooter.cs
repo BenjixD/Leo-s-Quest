60d1920d8f1dfe8999a22c454adb6908ac9775e0
nansonzheng <nanson.zheng@mail.utoronto.ca>
2016-01-11 20:54:54 -0500

    Added Shooter script, created arrow projectile prefab & behaviour script

---
+++ Assets/Player/Shooter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter: MonoBehaviour {
+
+    GameObject prefab;
+    Animator parentAnim;
+
+	// Use this for initialization
+	void Start () {
+        prefab = Resources.Load<GameObject>("Prefabs/Arrow");
+
+        parentAnim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E) || Input.GetMouseButtonDown(0))
+        {
+            Vector3 dirMod = new Vector3();
+            float angleRad = Mathf.Deg2Rad * parentAnim.GetFloat("relativeAngle");
+            dirMod.y -= Mathf.Sin(angleRad)/4;
+            dirMod.x -= Mathf.Cos(angleRad)/4;
+        
+            GameObject projectile = Instantiate(prefab, transform.position + dirMod, Quaternion.identity) as GameObject;
+            projectile.transform.Rotate(0,0,parentAnim.GetFloat("relativeAngle") - 90);
+            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+            //rb.position = transform.position + new Vector3(0, 3);
+            //rb.velocity = new Vector3(0, 5);
+            rb.velocity = 4 * dirMod;
+            
+        }
+	
+	}
+}
---
+++ Assets/Resources/ProjectileBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileBehaviour : MonoBehaviour {
+
+    Rigidbody2D selfRB;
+
+	// Use this for initialization
+	void Start () {
+        selfRB = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, 30);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D()
+    {
+        selfRB.velocity = Vector2.zero;
+        Destroy(gameObject, 5);
+    }
+}
